bd88d5d64abdf2fd43e7131adb55b3029ebfa5d4
Zhiying Pan <zp@cs.ualberta.ca>
2019-01-08 09:32:40 -0700

    script

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ print(requests.__version__)
 r = requests.get("http://www.google.com")
 print(r.status_code)
 print(r.headers)
+
+r = requests.get("https://raw.githubusercontent.com/Katzesama/cmput404lab1/master/main.py")
+print(r.text)
